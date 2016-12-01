38728b8967aa2aa3a58fd73d6b0ba5fcf5af2304
Nehemiah Loury <nmloury@gmail.com>
2016-12-01 12:03:20 -0800

    Adding Min Coin Problems

---
+++ Answers/min_coins.py
@@ -0,0 +1,18 @@
+def check_sum(cache, coins, total):
+    best_coins_count = float('inf')
+    for coin in coins:
+        temp_diff = total - coin
+        if temp_diff < 0:
+            pass
+        else:
+            total_count = 1 + cache[temp_diff]
+            if total_count < best_coins_count:
+                best_coins_count = total_count
+    cache[total] = best_coins_count
+
+
+def min_coins(coins, coin_sum):
+    cache = {0: 0}
+    for i in range(1, coin_sum + 1):
+        check_sum(cache, coins, i)
+    return cache[coin_sum]
